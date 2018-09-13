b07ba6e1ef54399bbe1a6e565f330812bb94a37e
Ryan Gill <rdgill95@gmail.com>
2018-09-13 03:40:38 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,7 +3,6 @@ var playlist = {
   'My Bloody Valentine' : 'Sometimes',
   }
 function updatePlaylist(obj,key,value) {
-  playlist['Phil Ochs'] = 
-  return
-  playlist
+  playlist['Phil Ochs'] = `Here's to the State of Mississippi`
+  return playlist
 }
