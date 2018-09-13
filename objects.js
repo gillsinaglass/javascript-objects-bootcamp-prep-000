fa09cee1b20eead2ca6fc60913cf1aa649cb5eaa
Ryan Gill <rdgill95@gmail.com>
2018-09-13 03:38:57 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,8 @@
+var playlist = {
+  'Slowdive' : 'Alison',
+  'My Bloody Valentine' : 'Sometimes',
+  }
+function updatePlaylist(obj,key,value) {
+  return
+  playlist['Phil Ochs'] = `Here's to the State of Mississippi`;
+}
